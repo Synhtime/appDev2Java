315fd2005425041005279d0f40de7fb395f30886
Synhtime <synhtime@mail.ru>
2016-04-16 17:12:17 +0300

    created Idea project

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,38 @@
+import java.io.*;
+
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
     static char[] delimeters = {' ', '\0'};
-
     static char[] line = new char[128];
+    static File file1 = new File("D:\\a\\2.txt"); //создай, плз, у себя файл с аналогичным путем, чтобы тестить
+
+    public static void main(String[] args) throws IOException
+    {
+        
 
 
-    public static void main(String[] args) throws IOException {
-
-        char symbol= '\0';
-        System.out.println(isDelimeter(symbol));
-        System.out.println(fromConsole(line));
+        //System.out.println(readFromFile(file1.getPath(), 2)); //тестинг метода возвращения длины определенной строки из файла
+        //char symbol= '\0';
+        //System.out.print(isDelimeter(symbol));
 
     }
 
-    public static int fromConsole(char [] inputLine)throws IOException{
+    public static int fromConsole(char[] inputLine) throws IOException {
         int i = 0;
         BufferedReader myReader = new BufferedReader(new InputStreamReader(System.in));
         char inputChar;
-        for(i=0; i<127;i++){
-            inputChar = (char)(myReader.read());
-            if(inputChar=='\n'){
+        for (i = 0; i < 127; i++) {
+            inputChar = (char) (myReader.read());
+            if (inputChar == '\n') {
                 break;
             }
-            inputLine[i]=inputChar;
+            inputLine[i] = inputChar;
         }
-        inputLine[i]='\0';
+        inputLine[i] = '\0';
         return i;
     }
 
@@ -40,6 +43,20 @@ public class Main {
             }
         }
         return 0;
+    }
+
+    public static int readFromFile(String filepath, int lineIndex) throws IOException {
+        BufferedReader bfr = new BufferedReader(new FileReader(filepath));
+        String s;
+
+        int count = 0;
+
+        while ((s = bfr.readLine()) != null) {
+            count++;
+            if (count == lineIndex) break;
+        }
+
+        return s.length();
     }
 }
 
