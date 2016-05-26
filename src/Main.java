145d2697d0b523e37ce97e1c3fdc621674799186
Synhtime <synhtime@mail.ru>
2016-05-26 11:25:51 +0300

    Update Main.java
    Добавлен код из свежего Main.class

--- src/Main.java
+++ src/Main.java
@@ -1,100 +1,136 @@
-import java.io.*;
-import java.lang.*;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
-
-    static char[] delimeters = {' ', '\0'};
+    static char[] delimeters = new char[]{' ', '\u0000'};
     static char[] line = new char[128];
-    static File file1 = new File("D:\\a\\2.txt"); /*создай, плз, у себя файл с аналогичным путем,
-    чтобы тестить. в нем напиши какой-нибудь текст с разными словами и строками. Файл
-    обязательно должен быть в кодировке UTF-8 (в блокноте: "Файл"-"Сохранить как", там будут парметры кодировки),
-    иначе могут быть траблы с кириллицей*/
+    static File file1 = new File("/home/thebaldsoprano/IdeaProjects/testFile");
+
+    public Main() {
+    }
 
     public static void main(String[] args) throws IOException {
-
-        //char symbol= '\0'; //твой код
-        //System.out.print(isDelimeter(symbol)); //твой код
-
-        //System.out.println(readFromFile(file1, 3)); //тестинг метода возвращения длины определенной строки из файла
-        //System.out.println(countFileWords(file1));  //тестинг пересчета слов в файле
-        //toConsole(file1); //тестинг вывода содержимого файла на консоль
-
-
+        System.out.println(fromConsole(line));
+        System.out.println(countWords(line));
     }
 
     public static int fromConsole(char[] inputLine) throws IOException {
-        int i = 0;
+        boolean i = false;
         BufferedReader myReader = new BufferedReader(new InputStreamReader(System.in));
-        char inputChar;
-        for (i = 0; i < 127; i++) {
-            inputChar = (char) (myReader.read());
-            if (inputChar == '\n') {
+
+        int var4;
+        for(var4 = 0; var4 < 127; ++var4) {
+            char inputChar = (char)myReader.read();
+            if(inputChar == 10) {
                 break;
             }
-            inputLine[i] = inputChar;
+
+            inputLine[var4] = inputChar;
         }
-        inputLine[i] = '\0';
-        return i;
+
+        inputLine[var4] = 0;
+        return var4;
     }
 
-    public static int isDelimeter(char symbolToCompare) {
-        for (int i = 0; i < delimeters.length; i++) {
-            if (symbolToCompare == delimeters[i]) {
-                return 1;
+    public static int countWords(char[] inputLine) {
+        int i = 0;
+        byte q = 0;
+        int words = 0;
+
+        while(inputLine[i] != 0) {
+            if(q == 0) {
+                if(isDelimeter(inputLine[i])) {
+                    q = 1;
+                    ++i;
+                } else {
+                    q = 2;
+                    ++i;
+                }
+            } else if(q == 1) {
+                if(isDelimeter(inputLine[i])) {
+                    ++i;
+                } else {
+                    q = 2;
+                    ++i;
+                }
+            } else if(q == 2) {
+                if(isDelimeter(inputLine[i])) {
+                    q = 1;
+                    ++words;
+                    ++i;
+                } else {
+                    ++i;
+                }
             }
         }
-        return 0;
+
+        if(q == 2) {
+            ++words;
+        }
+
+        return words;
+    }
+
+    public static boolean isDelimeter(char symbolToCompare) {
+        for(int i = 0; i < delimeters.length; ++i) {
+            if(symbolToCompare == delimeters[i]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static int readFromFile(File file1, int lineIndex) throws IOException {
         BufferedReader bfr = new BufferedReader(new FileReader(file1.getPath()));
-        String s;
-
         int count = 0;
 
-        while ((s = bfr.readLine()) != null) {
-            count++;
-            if (count == lineIndex) break;
+        String s;
+        while((s = bfr.readLine()) != null) {
+            ++count;
+            if(count == lineIndex) {
+                break;
+            }
         }
-        //System.out.println(s);
-        return (s == null || s.equals("") || s.charAt(0) == 65279) ? 0 : s.length();// это проверка на говносимволы, которые приводят к ошибкам
+
+        return s != null && !s.equals("") && s.charAt(0) != '\ufeff'?s.length():0;
     }
 
-    public static int countFileWords(File file1) throws IOException   //счет количества слов в файле с использованием delimeters как разделителей
-    {
-        StringBuilder sb1 = new StringBuilder(new String(Files.readAllBytes(Paths.get(file1.getPath())))); //создаем строку из всего файла, затем закидываем ее в StringBuilder для удобства обращения
+    public static int countFileWords(File file1) throws IOException {
+        StringBuilder sb1 = new StringBuilder(new String(Files.readAllBytes(Paths.get(file1.getPath(), new String[0]))));
+        String delimetersRegex = "";
 
-        String delimetersRegex = "";  //создаем заготовку для шаблона Regex (замысловатая штука для поиска совпадений в строках)
-
-        for (int i = 0; i < delimeters.length; i++) {                               //записываем заготовку из кусков твоего массива
-            if (i == delimeters.length - 1) delimetersRegex += delimeters[i] + "+";
-            else delimetersRegex += delimeters[i] + "+|";
+        for(int pt1 = 0; pt1 < delimeters.length; ++pt1) {
+            if(pt1 == delimeters.length - 1) {
+                delimetersRegex = delimetersRegex + delimeters[pt1] + "+";
+            } else {
+                delimetersRegex = delimetersRegex + delimeters[pt1] + "+|";
+            }
         }
 
-        Pattern pt1 = Pattern.compile(delimetersRegex); // создаем правило regex на основе заготовки
+        Pattern var5 = Pattern.compile(delimetersRegex);
 
-        while (sb1.charAt(0) == 65279 || (Character.toString(sb1.charAt(0))).matches(delimetersRegex)) //это для "Предусмотреть ситуацию, когда файл не содержит символов, отличных от разделителей. "
-        {
+        while(sb1.charAt(0) == '\ufeff' || Character.toString(sb1.charAt(0)).matches(delimetersRegex)) {
             sb1.deleteCharAt(0);
         }
 
-        String[] splittedString = pt1.split(sb1); //разделяем файл по шаблону Regex, полученные слова пихаем в массив
-        //for (String c : splittedString) System.out.println(c); //тестинг посредством вывода слов полученных после сплита
-
+        String[] splittedString = var5.split(sb1);
         return splittedString.length;
     }
 
     public static int toConsole(File file1) throws IOException {
-        System.out.println(new String(Files.readAllBytes(Paths.get(file1.getPath()))));
+        System.out.println(new String(Files.readAllBytes(Paths.get(file1.getPath(), new String[0]))));
         return countFileWords(file1);
     }
 }
-
-
